19cb7393e9fb37af2ce83bf6bc0f002867b3c7af
jeong <ibory1220@naver.com>
2022-02-15 06:33:13 +0900

    daily algorithm

--- 효율성/연속부분수열.js
+++ 효율성/연속부분수열.js
@@ -1,24 +1,17 @@
-function solution(arr, m) {
-  let p = 0,
-    q = 1;
-  let sum;
-  let answer = [];
-  while (p < arr.length && q < arr.length) {
-    sum = arr[p];
-    sum += arr[q];
-    console.log(sum);
-    if (sum === 6) {
-      const brr = arr.slice(p, q + 1);
-      answer([...brr]);
-      p++;
-      q = p + 1;
-    } else {
-      q++;
+function solution(m, arr) {
+  let answer = 0,
+    lt = 0,
+    sum = 0;
+  for (let rt = 0; rt < arr.length; rt++) {
+    sum += arr[rt];
+    if (sum === m) answer++;
+    while (sum >= m) {
+      sum -= arr[lt++];
+      if (sum === m) answer++;
     }
   }
   return answer;
 }
 
-let arr = [1, 2, 1, 3, 1, 1, 1, 2];
-let M = 6;
-console.log(solution(arr, 6));
+let a = [1, 2, 1, 3, 1, 1, 1, 2];
+console.log(solution(6, a));
