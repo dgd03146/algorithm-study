8a67e868d527b226dedc8d263393975ef5d9ec5f
jeong <ibory1220@naver.com>
2022-08-09 10:31:35 +0900

    Add daily algorithm

---
+++ Programmers/greedy/구명보트.js
@@ -0,0 +1,23 @@
+function solution(people, limit) {
+  var answer = 0;
+  people.sort((a, b) => b - a);
+
+  let l = 0;
+  let r = people.length - 1;
+
+  while (l < r) {
+    let sum = people[l] + people[r];
+    if (sum > limit) {
+      l++;
+    } else {
+      l++;
+      r--;
+    }
+    answer++;
+  }
+  if (l == r) answer++;
+
+  return answer;
+}
+
+console.log(solution([70, 50, 80, 50], 100));
