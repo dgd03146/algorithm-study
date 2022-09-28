5779e487837886ca9ee2ac40760f750768f6101d
jeong <ibory1220@naver.com>
2022-09-28 19:56:59 +0900

    daily algorithm

--- LeetCode/Arrays101/Squares-of-a-Sorted-Array.js
+++ LeetCode/Arrays101/Squares-of-a-Sorted-Array.js
@@ -1,8 +1,22 @@
 const nums = [-4, -1, 0, 3, 10];
-const sortedSquares = function (nums) {
-  const sortedSquares = nums.map((num) => num * num);
-  return sortedSquares.sort(function (a, b) {
-    return a - b;
-  });
+var sortedSquares = function (nums) {
+  let l = 0;
+  let r = nums.length - 1;
+  let squares = [];
+
+  while (l <= r) {
+    const ls = nums[l] * nums[l];
+    const rs = nums[r] * nums[r];
+
+    if (ls < rs) {
+      squares.push(rs);
+      r--;
+    } else {
+      squares.push(ls);
+      l++;
+    }
+  }
+
+  return squares.reverse();
 };
 console.log(sortedSquares(nums));
